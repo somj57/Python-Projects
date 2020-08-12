0edff38c58d87a177a9c8266f393a56e75da1a72
Somya Jaiswal <59413394+somj57@users.noreply.github.com>
2020-08-12 08:53:30 +0530

    For Sending bulk email at once

---
+++ Bulk Emails/bulk_email.py
@@ -0,0 +1,26 @@
+'''
+install pandas,smpt
+go to securities in email and allow unknown apps to login
+make a excel file as email.xlsx and store emails in it 
+
+
+
+'''
+import pandas as pd
+import smtplib
+
+SenderAddress = "Sender Email Address"
+password = "Password of sender"
+
+e = pd.read_excel("email.xlsx")
+emails = e['Emails'].values
+server = smtplib.SMTP("smtp.gmail.com",587)
+server.starttls()
+server.login(SenderAddress,password)
+msg = "Hellow this is bulk email from python2"
+subject = "Python"
+body = "Subject : {}\n\n{}".format(subject,msg)
+for email in emails:
+	server.sendmail(SenderAddress,email,body)
+	print("Email send . . . . .")
+server.quit() 
