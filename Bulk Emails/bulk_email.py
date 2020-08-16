0bb4648628ac9df439ea4cee35ff09e7eac2540c
Somya Jaiswal <59413394+somj57@users.noreply.github.com>
2020-08-16 11:04:02 +0530

    Update bulk_email.py

--- Bulk Emails/bulk_email.py
+++ Bulk Emails/bulk_email.py
@@ -20,16 +20,7 @@ server.login(SenderAddress,password)
 msg = "Hellow this is bulk email from python2"
 subject = "Python"
 body = "Subject : {}\n\n{}".format(subject,msg)
-#Adding_Attachment
-files = ['Files names']
-for file in files:	
-	with open(file,'rb') as f:
-		file_data = f.read()
-		file_type = imghdr.what(f.name)
-		file_name = f.name
-	email.add_attachment(file_data, maintype='image', subtype=file_type,filename=file_name )
-	print('Attachment Added . . . .')
-#Sending_message
+
 
 for email in emails:
 	server.sendmail(SenderAddress,email,body)
