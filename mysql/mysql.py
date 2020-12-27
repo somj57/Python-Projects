85069d54a5a8faefbcd87fa30be0c200ead4d3db
Somya Jaiswal <59413394+somj57@users.noreply.github.com>
2020-12-27 08:44:31 +0530

    Simple mysql connection scripts🚀

---
+++ mysql/mysql.py
@@ -0,0 +1,7 @@
+import mysql.connector
+con = mysql.connector.connect(
+	user= "somya",
+	password ="SAwFCJTqaLgRE89L",
+	host="localhost",
+	database = "somya"
+)
---
+++ mysql/start.py
@@ -0,0 +1,11 @@
+'''
+pip3  install mysql-connector-python
+'''
+
+import mysql.connector
+con = mysql.connector.connect(
+user= "somya",
+password ="SAwFCJTqaLgRE89L",
+host="108.167.140.122",
+database = "somya"
+)
