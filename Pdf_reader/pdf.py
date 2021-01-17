97e4c9c2b03333ad14291c2d36f3c4f734cc228d
Somya Jaiswal <59413394+somj57@users.noreply.github.com>
2021-01-17 06:43:26 +0530

    Update pdf.py

--- Pdf_reader/pdf.py
+++ Pdf_reader/pdf.py
@@ -1,5 +1,6 @@
 import PyPDF2
 
+# Write the name of the pdf and if which color format you want to load it to the memory
 with open('dummy.pdf', 'rb') as file:
 	reader = PyPDF2.PdfFileReader(file)
 	print(reader.numPages)
